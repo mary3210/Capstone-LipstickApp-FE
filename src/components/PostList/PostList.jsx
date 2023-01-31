a227b0e30ba59c72d54af7fc4d365aaa3c1e90ad
mary3210 <maryrodri3210@gmail.com>
2023-01-31 10:18:53 -0600

    populates post.name in PostList

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -22,7 +22,7 @@ const PostList = () => {
   return (
     <>
       <h2>Posts in Lists</h2>
-      <ul>{posts && posts.map((post) => <li> {post.review} </li>)}</ul>
+      <ul>{posts && posts.map((post) => <div> <img alt={post.tage} src={post.image}/> {post.name} {post.rating} </div>)}</ul>
     </>
   );
 };
