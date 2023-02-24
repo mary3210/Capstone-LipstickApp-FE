113581ddc58921146d0526b1c4983f470b4341fe
mary3210 <maryrodri3210@gmail.com>
2023-02-23 20:18:34 -0600

    change url base link

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -5,7 +5,7 @@ import HeartRating from "../HeartRating";
 const PostList = () => {
   const [posts, setPosts] = useState([]);
 
-  const BASE_URL = "http://localhost:8000";
+  const BASE_URL = "https://murmuring-lowlands-71026.herokuapp.com";
 
   const getPosts = async () => {
     try {
